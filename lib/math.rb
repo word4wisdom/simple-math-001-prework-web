fdd65f85ff3c6e4205ac7affcdfcd3cab0c13d7a
word4wisdom <WISDOMBTHEGAME@GMAIL.COM>
2015-11-22 18:47:26 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,53 @@
 def addition(num1, num2)
+  return num1 + num2
+
 end
+
+addition(6, 3)
+
 
 def subtraction(num1, num2)
+   num1 - num2
 end
+
+subtraction(10, 5)
 
 def division(num1, num2)
+  num1 / num2
+
 end
+
+division(50, 2)
+
 
 def multiplication(num1, num2)
+ num1 * num2
+
 end
+
+multiplication(4, 30)
+
 
 def modulo(num1, num2)
+  num1 % num2
+
 end
+
+modulo(34, 5)
 
 def square_root(num)
+  num / 9
+
 end
 
+ square_root(81)
+
+
+
 def order_of_operation(num1, num2, num3, num4)
+
+    num1 + ((num2 * num3) / num4)
   #Hint:  __ + (( __ * __ ) / __ )
 end
+
+order_of_operation(7, 43 , 23, 83)
